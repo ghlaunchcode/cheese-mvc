9e02c8226e2a2073015a2b88e694e76294d2c402
polarysekt <geoff@gd2>
2017-11-12 19:59:15 -0600

    Initial Project

---
+++ src/test/java/uX/kb3c/cheesemvc/CheeseMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package uX.kb3c.cheesemvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheeseMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
